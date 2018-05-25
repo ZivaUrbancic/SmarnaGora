dfdf484cc11542f0c26c6702693f140718decfa0
Živa Urbančič <ziva.urbancic@student.fmf.uni-lj.si>
2018-05-25 17:20:52 +0200

    adding discrete_gradient_vector_field.py

---
+++ discrete_gradient_vector_field.py
@@ -0,0 +1,635 @@
+# =============================================================================
+# Discrete Gradient Vector Field
+# =====================================================================@015087=
+# 1. podnaloga
+# We are given a Simplicial Complex as a set of its top-dimensional simplices.
+# Define a function random_discrete_gradient_vector_field, which accepts a
+# simplicial complex and returns random discrete gradient vector field on it.
+# 
+# The field should be returned as a tuple V, C. The V must be a dictionary
+# representing arrows in the discrete gradient vector field and C must be a
+# list of critical simplices.
+# 
+# Test your implementation on several simplicial complexes, such as torus and
+# sphere.
+# 
+# For example:
+# 
+#     >>> random_discrete_gradient_vector_field(set([1, 2, 3]))
+#     ({(1,2): (1,2,3), (1): (1,3), (3): (2,3)}, [(2,)])
+# =============================================================================
+import itertools as its
+
+
+def generate_all_sxs(K):
+    sxi = set()
+    for sx in K:
+        n = len(sx)
+        for i in range(n):
+            sxi = sxi.union(its.combinations(sx, i + 1))
+    return sxi
+
+
+def is_contained(sx1, sx2):
+    return all([i in sx2 for i in sx1])
+
+
+def is_highest_dim(all_sx_K):
+    max_dim = set()
+    for sx in all_sx_K:
+        if len(sx) > len(max_dim):
+            max_dim = sx
+    return max_dim
+
+
+def free_face(all_sx_K):
+    for sx1 in all_sx_K:
+        st = 0
+        fathers = []
+        for sx2 in all_sx_K:
+            if sx1 != sx2 and is_contained(sx1, sx2):
+                st += 1
+                fathers.append(sx2)
+        if st == 1:
+            return sx1, fathers[0]
+    return None
+
+
+def generate_vector_field(K):
+    V = {}
+    C = []
+    all_sxs = generate_all_sxs(K)
+    while all_sxs:
+        if free_face(all_sxs) is not None:
+            alfa, beta = free_face(all_sxs)
+            V[alfa] = beta
+            all_sxs.remove(alfa)
+            all_sxs.remove(beta)
+        else:
+            gamma = is_highest_dim(all_sxs)
+            C.append(gamma)
+            all_sxs.remove(gamma)
+    return V, C
+
+
+
+# =====================================================================@015088=
+# 2. podnaloga
+# Define a function max_paths that returns a list of maximal paths in a
+# simplicial complex equiped with a discrete gradient vector field.  
+# 
+# The path must be given as a list of simplices along path in a discrete
+# gradient vector field. 
+# 
+# For example:
+# 
+#     >>> max_paths(set([1, 2, 3]), ({(1,2): (1,2,3), (1): (1,3), (3): (2,3)}, [(2,)]))
+#     [[(1,2), (1,2,3), (1,3)], [(1,2), (1,2,3), (2,3)] [(1,), (1,3), (3,), (2,3), (2,)]]
+# =============================================================================
+
+# =====================================================================@015089=
+# 3. podnaloga
+# Define a function cancel that accepts a discrete gradient vector field and
+# a path connecting two critical simplices. It must cancel these two
+# critical simplices and return modified discrete gradient vector field.
+# 
+# If cancelation is not possible it should raise exception.
+# =============================================================================
+
+# =====================================================================@015092=
+# 4. podnaloga
+# Use the methods above to compute a discrete gradient vector field on a torus
+# with as little critical simplices as possible. 
+# 
+# First construct random discrete gradient vector field on it and then cancel
+# as many critical simplices as possible using the function cancel.
+# =============================================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ============================================================================@
+
+'Če vam Python sporoča, da je v tej vrstici sintaktična napaka,'
+'se napaka v resnici skriva v zadnjih vrsticah vaše kode.'
+
+'Kode od tu naprej NE SPREMINJAJTE!'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import io, json, os, re, sys, shutil, traceback, urllib.error, urllib.request
+
+
+from contextlib import contextmanager
+
+class Check:
+    @staticmethod
+    def has_solution(part):
+        return part['solution'].strip() != ''
+
+    @staticmethod
+    def initialize(parts):
+        Check.parts = parts
+        for part in Check.parts:
+            part['valid'] = True
+            part['feedback'] = []
+            part['secret'] = []
+        Check.current_part = None
+        Check.part_counter = None
+
+    @staticmethod
+    def part():
+        if Check.part_counter is None:
+            Check.part_counter = 0
+        else:
+            Check.part_counter += 1
+        Check.current_part = Check.parts[Check.part_counter]
+        return Check.has_solution(Check.current_part)
+
+    @staticmethod
+    def feedback(message, *args, **kwargs):
+        Check.current_part['feedback'].append(message.format(*args, **kwargs))
+
+    @staticmethod
+    def error(message, *args, **kwargs):
+        Check.current_part['valid'] = False
+        Check.feedback(message, *args, **kwargs)
+
+    @staticmethod
+    def clean(x, digits=6, typed=False):
+        t = type(x)
+        if t is float:
+            x = round(x, digits)
+            # Since -0.0 differs from 0.0 even after rounding,
+            # we change it to 0.0 abusing the fact it behaves as False.
+            v = x if x else 0.0
+        elif t is complex:
+            v = complex(Check.clean(x.real, digits, typed), Check.clean(x.imag, digits, typed))
+        elif t is list:
+            v = list([Check.clean(y, digits, typed) for y in x])
+        elif t is tuple:
+            v = tuple([Check.clean(y, digits, typed) for y in x])
+        elif t is dict:
+            v = sorted([(Check.clean(k, digits, typed), Check.clean(v, digits, typed)) for (k, v) in x.items()])
+        elif t is set:
+            v = sorted([Check.clean(y, digits, typed) for y in x])
+        else:
+            v = x
+        return (t, v) if typed else v
+
+    @staticmethod
+    def secret(x, hint=None, clean=None):
+        clean = clean or Check.clean
+        Check.current_part['secret'].append((str(clean(x)), hint))
+
+    @staticmethod
+    def equal(expression, expected_result, clean=None, env={}):
+        local_env = locals()
+        local_env.update(env)
+        clean = clean or Check.clean
+        actual_result = eval(expression, globals(), local_env)
+        if clean(actual_result) != clean(expected_result):
+            Check.error('Izraz {0} vrne {1!r} namesto {2!r}.',
+                        expression, actual_result, expected_result)
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def run(statements, expected_state, clean=None, env={}):
+        code = "\n".join(statements)
+        statements = "  >>> " + "\n  >>> ".join(statements)
+        s = {}
+        s.update(env)
+        clean = clean or Check.clean
+        exec(code, globals(), s)
+        errors = []
+        for (x, v) in expected_state.items():
+            if x not in s:
+                errors.append('morajo nastaviti spremenljivko {0}, vendar je ne'.format(x))
+            elif clean(s[x]) != clean(v):
+                errors.append('nastavijo {0} na {1!r} namesto na {2!r}'.format(x, s[x], v))
+        if errors:
+            Check.error('Ukazi\n{0}\n{1}.', statements,  ";\n".join(errors))
+            return False
+        else:
+            return True
+
+    @staticmethod
+    @contextmanager
+    def in_file(filename, content, encoding=None):
+        with open(filename, 'w', encoding=encoding) as f:
+            for line in content:
+                print(line, file=f)
+        old_feedback = Check.current_part['feedback'][:]
+        yield
+        new_feedback = Check.current_part['feedback'][len(old_feedback):]
+        Check.current_part['feedback'] = old_feedback
+        if new_feedback:
+            new_feedback = ['\n    '.join(error.split('\n')) for error in new_feedback]
+            Check.error('Pri vhodni datoteki {0} z vsebino\n  {1}\nso se pojavile naslednje napake:\n- {2}', filename, '\n  '.join(content), '\n- '.join(new_feedback))
+
+    @staticmethod
+    @contextmanager
+    def input(content, encoding=None):
+        old_stdin = sys.stdin
+        old_feedback = Check.current_part['feedback'][:]
+        sys.stdin = io.StringIO('\n'.join(content))
+        try:
+            yield
+        finally:
+            sys.stdin = old_stdin
+        new_feedback = Check.current_part['feedback'][len(old_feedback):]
+        Check.current_part['feedback'] = old_feedback
+        if new_feedback:
+            new_feedback = ['\n  '.join(error.split('\n')) for error in new_feedback]
+            Check.error('Pri vhodu\n  {0}\nso se pojavile naslednje napake:\n- {1}', '\n  '.join(content), '\n- '.join(new_feedback))
+
+    @staticmethod
+    def out_file(filename, content, encoding=None):
+        with open(filename, encoding=encoding) as f:
+            out_lines = f.readlines()
+        equal, diff, line_width = Check.difflines(out_lines, content)
+        if equal:
+            return True
+        else:
+            Check.error('Izhodna datoteka {0}\n  je enaka{1}  namesto:\n  {2}', filename, (line_width - 7) * ' ', '\n  '.join(diff))
+            return False
+
+    @staticmethod
+    def output(expression, content, use_globals=False):
+        old_stdout = sys.stdout
+        sys.stdout = io.StringIO()
+        try:
+            def visible_input(prompt):
+                inp = input(prompt)
+                print(inp)
+                return inp
+            exec(expression, globals() if use_globals else {'input': visible_input})
+        finally:
+            output = sys.stdout.getvalue().strip().splitlines()
+            sys.stdout = old_stdout
+        equal, diff, line_width = Check.difflines(output, content)
+        if equal:
+            return True
+        else:
+            Check.error('Program izpiše{0}  namesto:\n  {1}', (line_width - 13) * ' ', '\n  '.join(diff))
+            return False
+
+    @staticmethod
+    def difflines(actual_lines, expected_lines):
+        actual_len, expected_len = len(actual_lines), len(expected_lines)
+        if actual_len < expected_len:
+            actual_lines += (expected_len - actual_len) * ['\n']
+        else:
+            expected_lines += (actual_len - expected_len) * ['\n']
+        equal = True
+        line_width = max(len(actual_line.rstrip()) for actual_line in actual_lines + ['Program izpiše'])
+        diff = []
+        for out, given in zip(actual_lines, expected_lines):
+            out, given = out.rstrip(), given.rstrip()
+            if out != given:
+                equal = False
+            diff.append('{0} {1} {2}'.format(out.ljust(line_width), '|' if out == given else '*', given))
+        return equal, diff, line_width
+
+    @staticmethod
+    def generator(expression, expected_values, should_stop=False, further_iter=0, env={}, clean=None):
+        from types import GeneratorType
+        local_env = locals()
+        local_env.update(env)
+        clean = clean or Check.clean
+        gen = eval(expression, globals(), local_env)
+        if not isinstance(gen, GeneratorType):
+            Check.error("Izraz {0} ni generator.", expression)
+            return False
+
+        try:
+            for iteration, expected_value in enumerate(expected_values):
+                actual_value = next(gen)
+                if clean(actual_value) != clean(expected_value):
+                    Check.error("Vrednost #{0}, ki jo vrne generator {1} je {2!r} namesto {3!r}.",
+                                iteration, expression, actual_value, expected_value)
+                    return False
+            for _ in range(further_iter):
+                next(gen)  # we will not validate it
+        except StopIteration:
+            Check.error("Generator {0} se prehitro izteče.", expression)
+            return False
+        
+        if should_stop:
+            try:
+                next(gen)
+                Check.error("Generator {0} se ne izteče (dovolj zgodaj).", expression)
+            except StopIteration:
+                pass  # this is fine
+        return True
+
+    @staticmethod
+    def summarize():
+        for i, part in enumerate(Check.parts):
+            if not Check.has_solution(part):
+                print('{0}. podnaloga je brez rešitve.'.format(i + 1))
+            elif not part['valid']:
+                print('{0}. podnaloga nima veljavne rešitve.'.format(i + 1))
+            else:
+                print('{0}. podnaloga ima veljavno rešitev.'.format(i + 1))
+            for message in part['feedback']:
+                print('  - {0}'.format('\n    '.join(message.splitlines())))
+
+
+def _validate_current_file():
+    def extract_parts(filename):
+        with open(filename, encoding='utf-8') as f:
+            source = f.read()
+        part_regex = re.compile(
+            r'# =+@(?P<part>\d+)=\s*\n' # beginning of header
+            r'(\s*#( [^\n]*)?\n)+?'     # description
+            r'\s*# =+\s*?\n'            # end of header
+            r'(?P<solution>.*?)'        # solution
+            r'(?=\n\s*# =+@)',          # beginning of next part
+            flags=re.DOTALL | re.MULTILINE
+        )
+        parts = [{
+            'part': int(match.group('part')),
+            'solution': match.group('solution')
+        } for match in part_regex.finditer(source)]
+        # The last solution extends all the way to the validation code,
+        # so we strip any trailing whitespace from it.
+        parts[-1]['solution'] = parts[-1]['solution'].rstrip()
+        return parts
+
+    def backup(filename):
+        backup_filename = None
+        suffix = 1
+        while not backup_filename or os.path.exists(backup_filename):
+            backup_filename = '{0}.{1}'.format(filename, suffix)
+            suffix += 1
+        shutil.copy(filename, backup_filename)
+        return backup_filename
+
+    def submit_parts(parts, url, token):
+        submitted_parts = []
+        for part in parts:
+            if Check.has_solution(part):
+                submitted_part = {
+                    'part': part['part'],
+                    'solution': part['solution'],
+                    'valid': part['valid'],
+                    'secret': [x for (x, _) in part['secret']],
+                    'feedback': json.dumps(part['feedback']),
+                }
+                if 'token' in part:
+                    submitted_part['token'] = part['token']
+                submitted_parts.append(submitted_part)
+        data = json.dumps(submitted_parts).encode('utf-8')
+        headers = {
+            'Authorization': token,
+            'content-type': 'application/json'
+        }
+        request = urllib.request.Request(url, data=data, headers=headers)
+        response = urllib.request.urlopen(request)
+        return json.loads(response.read().decode('utf-8'))
+
+    def update_attempts(old_parts, response):
+        updates = {}
+        for part in response['attempts']:
+            part['feedback'] = json.loads(part['feedback'])
+            updates[part['part']] = part
+        for part in old_parts:
+            valid_before = part['valid']
+            part.update(updates.get(part['part'], {}))
+            valid_after = part['valid']
+            if valid_before and not valid_after:
+                wrong_index = response['wrong_indices'].get(str(part['part']))
+                if wrong_index is not None:
+                    hint = part['secret'][wrong_index][1]
+                    if hint:
+                        part['feedback'].append('Namig: {}'.format(hint))
+
+
+    filename = os.path.abspath(sys.argv[0])
+    file_parts = extract_parts(filename)
+    Check.initialize(file_parts)
+
+    if Check.part():
+        Check.current_part['token'] = 'eyJwYXJ0IjoxNTA4NywidXNlciI6Mjc4M30:1fM82O:eKPuVx-VDFbWPUkLAwqcF7ngjG0'
+        try:
+            pass
+            # Check.equal('multiply(3, 7)', 21)
+            # Check.equal('multiply(6, 7)', 42)
+            # Check.equal('multiply(10, 10)', 100)
+            # Check.secret(multiply(100, 100))
+            # Check.secret(multiply(500, 123))
+        except:
+            Check.error("Testi sprožijo izjemo\n  {0}",
+                        "\n  ".join(traceback.format_exc().split("\n"))[:-2])
+
+    if Check.part():
+        Check.current_part['token'] = 'eyJwYXJ0IjoxNTA4OCwidXNlciI6Mjc4M30:1fM82O:Md34rZZyHn5zUqJwCqsjMJxhIGw'
+        try:
+            pass
+            ""
+            # Check.equal('multiply(3, 7)', 21)
+            # Check.equal('multiply(6, 7)', 42)
+            # Check.equal('multiply(10, 10)', 100)
+            # Check.secret(multiply(100, 100))
+            # Check.secret(multiply(500, 123))
+        except:
+            Check.error("Testi sprožijo izjemo\n  {0}",
+                        "\n  ".join(traceback.format_exc().split("\n"))[:-2])
+
+    if Check.part():
+        Check.current_part['token'] = 'eyJwYXJ0IjoxNTA4OSwidXNlciI6Mjc4M30:1fM82O:H7eiU9smz-mwdFMvPPIFoE355Fs'
+        try:
+            pass
+            ""
+            # Check.equal('multiply(3, 7)', 21)
+            # Check.equal('multiply(6, 7)', 42)
+            # Check.equal('multiply(10, 10)', 100)
+            # Check.secret(multiply(100, 100))
+            # Check.secret(multiply(500, 123))
+        except:
+            Check.error("Testi sprožijo izjemo\n  {0}",
+                        "\n  ".join(traceback.format_exc().split("\n"))[:-2])
+
+    if Check.part():
+        Check.current_part['token'] = 'eyJwYXJ0IjoxNTA5MiwidXNlciI6Mjc4M30:1fM82O:vR_KJKGDZXnlivQIq-yt6XthpzM'
+        try:
+            ""
+            pass
+            
+            # Check.equal('multiply(3, 7)', 21)
+            # Check.equal('multiply(6, 7)', 42)
+            # Check.equal('multiply(10, 10)', 100)
+            # Check.secret(multiply(100, 100))
+            # Check.secret(multiply(500, 123))
+        except:
+            Check.error("Testi sprožijo izjemo\n  {0}",
+                        "\n  ".join(traceback.format_exc().split("\n"))[:-2])
+
+    print('Shranjujem rešitve na strežnik... ', end="")
+    try:
+        url = 'https://www.projekt-tomo.si/api/attempts/submit/'
+        token = 'Token bf4dd700761c45d67c6044a64ea4029b26e2758d'
+        response = submit_parts(Check.parts, url, token)
+    except urllib.error.URLError:
+        print('PRI SHRANJEVANJU JE PRIŠLO DO NAPAKE! Poskusite znova.')
+    else:
+        print('Rešitve so shranjene.')
+        update_attempts(Check.parts, response)
+        if 'update' in response:
+            print('Posodabljam datoteko... ', end="")
+            backup_filename = backup(filename)
+            with open(__file__, 'w', encoding='utf-8') as f:
+                f.write(response['update'])
+            print('Stara datoteka je bila preimenovana v {0}.'.format(backup_filename))
+            print('Če se datoteka v urejevalniku ni osvežila, jo zaprite ter ponovno odprite.')
+    Check.summarize()
+
+if __name__ == '__main__':
+    _validate_current_file()
